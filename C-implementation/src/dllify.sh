1dad72d8137e132343f9cefd2adece9b1639422b
Ram <ramsir3@gmail.com>
2016-11-13 05:23:25 -0500

    updated dllify.sh

--- C-implementation/src/dllify.sh
+++ C-implementation/src/dllify.sh
@@ -2,9 +2,9 @@
 
 CFILES=(lacoperon.c engine.c sir.c)
 
-for f in `echo ${CFILES[*]}`; do gcc -c -DBUILD_DLL -fPIC "$f"; done;
+for f in `echo ${CFILES[*]}`; do gcc -c -Wall -Werror -fpic "$f"; done;
 
-for f in `echo ${CFILES[*]}`; do gcc -shared -o `echo ${f/.c/.dll}` `echo ${f/.c/.o}`; done;
+for f in `echo ${CFILES[*]}`; do gcc -shared -o `echo ${f/.c/.so}` `echo ${f/.c/.o}`; done;
 
-mv *.dll ../lib/;
+mv *.so ../lib/;
 rm *.o;
